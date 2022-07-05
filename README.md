acf103aa6b255d16bf3c77b6727e6d48a4a18637
Jia-Han Wu <jiahan.wu@icloud.com>
2022-07-05 23:03:59 +0800

    Update index.tsx, Home.module.css and README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# blog
+# Title
+
+## Section 1
+
+Content1...
+
+## Section 2
+
+Content2...
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,34 @@ import { GetStaticProps, NextPage } from 'next';
 import * as fs from 'fs';
 import * as path from 'path';
 import ReactMarkdown from 'react-markdown';
+import styles from '../styles/Home.module.css'
 
 interface HomeProps {
   markdown: string;
 }
 
 const Home: NextPage<HomeProps> = ({ markdown }) => {
-  return (
-    <ReactMarkdown>#hello world ##S1 ##S2</ReactMarkdown>
-  )
+  return <>
+    <header className={styles["header"]}>
+      <div>Blog</div>
+      <div>
+        <ul>
+          <li>Item1</li>
+          <li>Item2</li>
+          <li>Item3</li>
+        </ul>
+      </div>
+    </header>
+    <main className={styles["main"]}>
+      <div className={styles["table-of-contents"]}>
+        Table of Contents
+      </div>
+      <div>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+    </main>
+    <footer className={styles["footer"]}>© 2022 Jia-Han Wu</footer>
+  </>
 };
 
 export const getStaticProps: GetStaticProps<HomeProps> =
