d5a040c36c9834beed84fbe2129a27a6b0df89c1
Jia-Han Wu <jiahan.wu@icloud.com>
2022-07-06 10:16:27 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { GetStaticProps, NextPage } from 'next';
+import { NodeWalkingStep, Parser } from 'commonmark';
 import * as fs from 'fs';
+import { GetStaticProps, NextPage } from 'next';
 import * as path from 'path';
 import ReactMarkdown from 'react-markdown';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 interface HomeProps {
   markdown: string;
@@ -36,6 +37,19 @@ export const getStaticProps: GetStaticProps<HomeProps> =
   async () => {
     const readmeFilePath = path.join(process.cwd(), "README.md")
     const markdown = fs.readFileSync(readmeFilePath, { encoding: 'utf-8' })
+
+    // TODO: - Task 1
+    const parser = new Parser()
+    const rootNode = parser.parse(markdown)
+    const walker = rootNode.walker()
+    let event: NodeWalkingStep
+    while (event = walker.next()) {
+      console.log("---------------")
+      console.log(event.node.type)
+      console.log(event.node.literal)
+      console.log("---------------")
+    }
+
     return {
       props: {
         markdown,
