ea519494a0f4c76174b4b1fb23669c4633c59190
Jia-Han Wu <jiahan.wu@icloud.com>
2022-07-06 21:51:03 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,40 @@
-import { NodeWalkingStep, Parser } from 'commonmark';
+import { Node, NodeWalkingStep, Parser } from 'commonmark';
 import * as fs from 'fs';
 import { GetStaticProps, NextPage } from 'next';
 import * as path from 'path';
 import ReactMarkdown from 'react-markdown';
 import styles from '../styles/Home.module.css';
 
-interface HomeProps {
-  markdown: string;
+interface Section {
+  level: number
+  text: string
 }
 
-const Home: NextPage<HomeProps> = ({ markdown }) => {
+interface HomeProps {
+  sections: Section[]
+  markdown: string
+}
+
+const Home: NextPage<HomeProps> = ({ sections, markdown }) => {
+  const sectionRows = sections.map((section) => {
+    return <li key={section.text}>{section.text}(Level: {section.level})</li>
+  })
   return <>
     <header className={styles["header"]}>
       <div>Blog</div>
       <div>
         <ul>
-          <li>Item1</li>
-          <li>Item2</li>
-          <li>Item3</li>
+          <li>Home</li>
+          <li>About</li>
         </ul>
       </div>
     </header>
     <main className={styles["main"]}>
       <div className={styles["table-of-contents"]}>
         Table of Contents
+        <ul>
+          {sectionRows}
+        </ul>
       </div>
       <div>
         <ReactMarkdown>{markdown}</ReactMarkdown>
@@ -36,22 +47,34 @@ const Home: NextPage<HomeProps> = ({ markdown }) => {
 export const getStaticProps: GetStaticProps<HomeProps> =
   async () => {
     const readmeFilePath = path.join(process.cwd(), "README.md")
+
     const markdown = fs.readFileSync(readmeFilePath, { encoding: 'utf-8' })
 
-    // TODO: - Task 1
+    const sections: Section[] = []
     const parser = new Parser()
     const rootNode = parser.parse(markdown)
     const walker = rootNode.walker()
-    let event: NodeWalkingStep
+    let event: NodeWalkingStep | null = null
+    let headingNode: Node | null = null
     while (event = walker.next()) {
-      console.log("---------------")
-      console.log(event.node.type)
-      console.log(event.node.literal)
-      console.log("---------------")
+      if (event.node.type === "heading" && headingNode !== null) {
+        headingNode = null
+      } else if (event.node.type === "text" && headingNode !== null) {
+        const level = headingNode.level
+        const text = event.node.literal ?? ""
+        const section = {
+          level,
+          text
+        }
+        sections.push(section)
+      } else if (event.node.type === "heading") {
+        headingNode = event.node
+      }
     }
 
     return {
       props: {
+        sections,
         markdown,
       },
     };
